f39e834328a8fab8b9f4bf17df8007a93edeed52
Kiran Babu <kiranbabu@MacBook-Pro-8.local>
2024-05-03 12:02:55 +0530

    trained bigram

---
+++ bigrams.py
@@ -0,0 +1,75 @@
+import torch 
+import torch.nn  as nn 
+from torch.nn import functional as F 
+
+# hyperparameters 
+batch_size=32
+block_size=8 
+max_iters=3000
+eval_interval = 300 
+learning_rate=1e-2 
+device='cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters=200 
+
+torch.manual_seed(200)
+
+with open('input.txt','r',encoding='utf-8') as f:
+    text=f.read()
+    
+# here are all the unique characters that occur in the text
+chars=sorted(list(set(text)))
+vocab_size=len(chars)
+
+# create a mapping from characters  to intergers
+stoi = {ch:i for i,ch in enumerate(chars)}
+itos ={i:ch for i,ch in enumerate(chars)}
+
+encode = lambda s:[stoi[c] for c in s] 
+decode = lambda l : "".join([itos[i] for i in l])
+
+# Train and test split 
+data=torch.tensor(encode(text),type=torch.long)
+n = int(0.9*len(data))
+train_data = data[:n]
+val_data=data[n:]
+
+# data loading 
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y 
+    data=train_data if split == 'train' else val_data
+    ix =torch.randint(len(data)-block_size,(batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix ])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x,y = x.to(device), y.to(device)
+    return x, y 
+
+@torch.no_grad
+def estimate_loss():
+    out={}
+    model.eval() 
+    for split in ['train','val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X,Y = get_batch(split)
+            logits,loss = model(X,Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train() 
+    return out 
+
+# Simple bigram model 
+class BigramLanguageModel(nn.Module):
+    
+    def __init__(self, vocab_size) :
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size,vocab_size)
+        
+    def forward(self,idx,targets=None):
+        
+        # idx and targets are both (B,T) tensors of integers
+        logits=self.token_embedding_table(idx) 
+        
+        if targets is None:
+            loss = None
+        else:
+            B
